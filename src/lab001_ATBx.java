9a9de3a89fc09a4469304711a23b96c6634a308d
Amit Rana <amitrana4788@gmail.com>
2025-01-07 02:54:19 +0530

    NEW

---
+++ src/lab001_ATBx.java
@@ -0,0 +1,5 @@
+public class lab001_ATBx {
+    public static void main(String[] args){
+        System.out.println("Welcome to CoreJava");
+    }
+}
---
+++ src/lab002_ATBx.java
@@ -0,0 +1,6 @@
+public class lab002_ATBx {
+    public static void main (String[] args){
+        System.out.println("Hello Java World");
+    }
+}
+
