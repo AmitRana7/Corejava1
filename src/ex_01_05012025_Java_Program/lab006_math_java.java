43872f017e1f1f042b4df6aade1b4247fe2391f6
Amit Rana <amitrana4788@gmail.com>
2025-01-15 02:24:29 +0530

    Hello commit

---
+++ src/ex_01_05012025_Java_Program/lab006_math_java.java
@@ -0,0 +1,15 @@
+package ex_01_05012025_Java_Program;
+
+public class lab006_math_java {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(3/2);
+        System.out.print(3/2);
+        // println ....>only integer part.....>no decimal
+        //   System.out.print(3/2);
+        // only 1 not 1.5 here
+    }
+}
