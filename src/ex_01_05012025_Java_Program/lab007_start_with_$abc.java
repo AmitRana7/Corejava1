b7b44f6a0e115d6a4657bdd1be89a4cf1fd1602e
Amit Rana <amitrana4788@gmail.com>
2025-01-15 22:09:41 +0530

    Hello commit33

---
+++ src/ex_01_05012025_Java_Program/lab007_start_with_$abc.java
@@ -0,0 +1,10 @@
+package ex_01_05012025_Java_Program;
+
+public class lab007_start_with_$abc {
+    //public class _$123abc
+   public static void main(String[] args) {
+       System.out.println("public class _$123abc start with only");
+
+   }
+
+}
