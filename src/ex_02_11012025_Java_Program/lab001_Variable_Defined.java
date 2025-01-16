8a066e51b6da9abb800a107d355dd5597544f942
Amit Rana <amitrana4788@gmail.com>
2025-01-16 23:30:24 +0530

    Hello commit33

---
+++ src/ex_02_11012025_Java_Program/lab001_Variable_Defined.java
@@ -0,0 +1,11 @@
+package ex_02_11012025_Java_Program;
+
+public class lab001_Variable_Defined {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println(a);
+        System.out.println(a+b);
+    }
+
+}
