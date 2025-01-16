37968416ce0806b7e4ced36fc3c9f628db92b127
Amit Rana <amitrana4788@gmail.com>
2025-01-16 23:49:59 +0530

    Hello commit33

---
+++ src/ex_02_11012025_Java_Program/lab001_VariableName_start.java
@@ -0,0 +1,12 @@
+package ex_02_11012025_Java_Program;
+
+public class lab001_VariableName_start {
+    public static void main(String[] args) {
+        int _ =  20;
+        int $ =  30;
+        int abc = 255;
+       // System.out.println(_); // underscore are allowed here
+        System.out.println($);
+        System.out.println(abc);
+    }
+}
