2f73ec1a0ef5561fb727de034eadea030b502a92
Amit Rana <amitrana4788@gmail.com>
2025-01-16 23:37:00 +0530

    Hello commit33

---
+++ src/ex_02_11012025_Java_Program/lab002_Variable_Value.java
@@ -0,0 +1,9 @@
+package ex_02_11012025_Java_Program;
+
+public class lab002_Variable_Value {
+    public static void main(String[] args) {
+        int age = 65;
+        age = 88;
+        System.out.println(age);
+    }
+}
