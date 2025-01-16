e3170c573a0815403334a3ac3c66148630afe5de
Amit Rana <amitrana4788@gmail.com>
2025-01-16 23:43:27 +0530

    Hello commit33

---
+++ src/ex_02_11012025_Java_Program/lab003_Variable_more.java
@@ -0,0 +1,10 @@
+package ex_02_11012025_Java_Program;
+
+public class lab003_Variable_more {
+    public static void main(String[] args) {
+        int age = 55;
+        age = age + 1;
+        age = 89;
+        System.out.println(age);
+    }
+}
