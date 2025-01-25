6fcbceea931b31fb7877a2861a68be39baf31794
Amit Rana <amitrana4788@gmail.com>
2025-01-25 22:10:51 +0530

    Question Ans. of Condition & Loops

---
+++ src/ex_04_23012025_Loops/lab009_Home_lec4.java
@@ -0,0 +1,12 @@
+package ex_04_23012025_Loops;
+
+public class lab009_Home_lec4 {
+    public static void main(String[] args) {
+        int n = 50;
+        for (int i=1; i<=n; i++){
+            if ( i % 2 ==0 ){   // Print all even number till n..*
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/ex_04_23012025_Loops/lab010_Home_lec4one.java
@@ -0,0 +1,12 @@
+package ex_04_23012025_Loops;
+
+public class lab010_Home_lec4one {
+    public static void main(String[] args) {
+        int n = 25;
+        for (int i=1; i<=n; i++){
+          if (i % 2 ==1){
+              System.out.println(i);   // Print all Odd number till n.
+          }
+        }
+    }
+}
