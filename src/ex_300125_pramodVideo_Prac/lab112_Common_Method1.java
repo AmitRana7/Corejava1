797c6039ae3e0c879b1fc289485e2ef40a4653cc
Amit Rana <amitrana4788@gmail.com>
2025-02-16 23:58:36 +0530

    String with common method

---
+++ src/ex_300125_pramodVideo_Prac/lab112_Common_Method1.java
@@ -0,0 +1,29 @@
+package ex_300125_pramodVideo_Prac;
+
+public class lab112_Common_Method1 {
+    public static void main(String[] args) {
+        String str1 = "Veer ";
+        String str2 = "Veer Rana";
+        System.out.println(str1.charAt(3));     //Character
+        System.out.println(str1.concat(str2));   //concatenation
+        System.out.println(str1.contains("Veer")); // contains
+        System.out.println(str1.equals("Veer "));
+        System.out.println(str1.equalsIgnoreCase("veer "));
+        System.out.println(str1.indexOf('r'));
+        System.out.println(str1.replace('e','E'));
+        System.out.println(str1.length());
+        System.out.println(str1.trim());
+        System.out.println(str1.toLowerCase());
+        System.out.println(str1.toUpperCase());
+        System.out.println(str1.startsWith("B"));
+        System.out.println(str1.endsWith("u"));
+
+
+
+
+
+
+
+
+    }
+}
