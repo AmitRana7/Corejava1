a4682d82b3d31106aa93939ab463e7fdfe38acff
Amit Rana <amitrana4788@gmail.com>
2025-02-16 09:02:23 +0530

    String with StringBuilder()

---
+++ src/ex_300125_pramodVideo_Prac/lab104_StringBuilder_insert.java
@@ -0,0 +1,9 @@
+package ex_300125_pramodVideo_Prac;
+
+public class lab104_StringBuilder_insert {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello World");
+        sb.insert(6, "Java ");
+        System.out.println(sb);
+    }
+}
---
+++ src/ex_300125_pramodVideo_Prac/lab105_stringBuilder_insert.java
@@ -0,0 +1,9 @@
+package ex_300125_pramodVideo_Prac;
+
+public class lab105_stringBuilder_insert {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("I Love Programming");
+        sb.insert(6," Java");
+        System.out.println(sb);
+    }
+}
