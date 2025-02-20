4566baddefa6ea4d88e851889288ae77ec7f88fe
Amit Rana <amitrana4788@gmail.com>
2025-02-20 23:17:12 +0530

    Arrays with 2D

---
+++ src/ex_300125_pramodVideo_Prac/lab106_StringBuffer_append.java
@@ -0,0 +1,8 @@
+package ex_300125_pramodVideo_Prac;
+
+public class lab106_StringBuffer_append {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello Java");
+        System.out.println(sb.append(" World"));
+    }
+}
