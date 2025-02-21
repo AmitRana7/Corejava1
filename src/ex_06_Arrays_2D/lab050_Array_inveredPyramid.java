7b972b8e88f5d3ddcd2fa3525cb9418dc5f74bee
Amit Rana <amitrana4788@gmail.com>
2025-02-21 15:52:51 +0530

    Arrays with pattern

---
+++ src/ex_06_Arrays_2D/lab050_Array_inveredPyramid.java
@@ -0,0 +1,18 @@
+package ex_06_Arrays_2D;
+
+public class lab050_Array_inveredPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = n; i >=1 ; i--) {
+            for (int j = 1; j < n ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=( 2 * i -1); j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
